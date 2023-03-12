ffdb8a80dea88dbc2765d3759718f9ce8a437d92
TiredACE <1125953304@qq.com>
2023-03-13 01:17:41 +0800

    2023-03-13 1:17

---
+++ SCM/单片机原理及应用教程/code/第1章/LED/LED/flash_led.c
@@ -0,0 +1,22 @@
+#include <reg52.h>
+#define uint unsigned int
+sbit D1 = P1^0;
+void delay(uint z);
+
+void main()
+{
+	while (1)
+	{
+		D1 = 0;
+		delay(500);
+		D1 = 1;
+		delay(500);
+	}
+}
+
+void delay(uint z)
+{
+	uint x, y;
+	for (x = z; x > 0; x --)
+		for (y = 110; y > 0; y --);
+}
